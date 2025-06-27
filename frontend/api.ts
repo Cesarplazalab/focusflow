65bb8ce9f1a762eb0672a46eb494c9c4d8e68817
Cesar Plaza <cesar.ugc.content@gmail.com>
2025-06-27 15:21:35 -0400

    Connected frontend to live backend

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://focusflow-backend-xpi5.onrender.com',
 });
 
 export default api;
+
